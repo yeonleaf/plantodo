f531d81a2b5093d1ab44655f0a187b9ab3675315
yeonleaf <hcjy97@naver.com>
2021-10-27 16:51:22 +0900

    Initial commit

---
+++ src/main/java/demo/plantodo/PlantodoApplication.java
@@ -0,0 +1,13 @@
+package demo.plantodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/plantodo/PlantodoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.plantodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
